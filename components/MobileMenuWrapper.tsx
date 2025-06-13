51d8edd03cf678d57c9499c4a00306ad512eadad
corlino <corlinoneill@gmail.com>
2025-06-12 17:54:12 -0700

    debug

--- components/MobileMenuWrapper.tsx
+++ components/MobileMenuWrapper.tsx
@@ -5,7 +5,7 @@ import { useIsMobile } from "@/hooks/use-mobile"
 
 export default function MobileMenuWrapper() {
     const isMobile = useIsMobile()
-
+    console.log("MobileMenuWrapper rendering; isMobile =", isMobile)
     if (!isMobile) return null
     return <MobileMenu />
 }

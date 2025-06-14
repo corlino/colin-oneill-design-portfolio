c64228366625001185d86729030201d4598db996
corlino <corlinoneill@gmail.com>
2025-06-13 17:19:42 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     return (
         <html lang="en" className="scroll-smooth">
             <body className={inter.className}>
-                <MobileMenuWrapper />
                 {children}
             </body>
         </html>

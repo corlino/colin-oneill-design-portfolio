9ffea846b878c7b6e9db22edaf868a813d9cda7b
pleach <corlinoneill@gmail.com>
2025-11-26 14:10:49 -0800

    Update page.tsx

--- app/projects/edwtproject/page.tsx
+++ app/projects/edwtproject/page.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image"
 import Link from "next/link"
 import { ArrowRight } from "lucide-react"
+import { ArrowLeft } from "lucide-react"
 
 
 import { Button } from "@/components/ui/button"

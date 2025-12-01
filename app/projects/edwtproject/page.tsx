3711d23dbc7c8f4752c99de5295fc3162f7f27d4
pleach <corlinoneill@gmail.com>
2025-12-01 09:54:40 -0800

    Update page.tsx

--- app/projects/edwtproject/page.tsx
+++ app/projects/edwtproject/page.tsx
@@ -4,7 +4,6 @@ import Image from "next/image"
 import Link from "next/link"
 import { ArrowRight } from "lucide-react"
 import { ArrowLeft } from "lucide-react"
-import Link from "next/link"
 import { ArrowUpRight } from "lucide-react"
 
 

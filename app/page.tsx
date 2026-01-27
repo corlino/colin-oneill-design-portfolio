97ec767ccdd836f4357d6925b63d53df5f34f218
pleach <corlinoneill@gmail.com>
2026-01-27 13:41:56 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -185,7 +185,7 @@ export default function HomePage() {
             {/* ===================== */}
             {/* Hero Section */}
             {/* ===================== */}
-            <section className="relative py-20 px-8 min-h-[80vh] sm:min-h-[90vh] md:min-h-[90vh]">
+            <section className="relative py-20 px-8 min-h-[95vh] sm:min-h-[90vh] md:min-h-[90vh]">
                 {/* Hero Background Image */}
                 <div className="absolute top-0 left-0 w-full h-full -z-10">
                     <Image
